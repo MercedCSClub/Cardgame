0f1bcfc9ccd11b35580e2e8be9e92ca3429ea871
Masterpc <admin@actuateideas.com>
2017-10-09 13:36:02 -0700

    first commit

---
+++ Assets/card server.py
@@ -0,0 +1,89 @@
+
+
+from flask import Flask,g,request
+import json,uuid
+app = Flask(__name__)
+serverData={}
+serverData["users"]=[]
+serverData["games"]=[]
+from random import randint
+
+matches={"dog":{"cat":2.0,"dog":1.0,"person":3.0,"dogCatcher":0.3},"cat":{"cat":1.0,"dog":0.5,"person":0.3,"dogCatcher":0.0},
+"person":{"cat":1.0,"dog":1.0,"person":1.0,"dogCatcher":0.3},"dogCatcher":{"cat":-0.3,"dog":3.0,"person":3.0,"dogCatcher":0}}
+class card():
+	def __init__(self,player,name,_class,power):
+		self.id=uuid.uuid4()
+		self.name=name
+		self._class=_class
+		self.power=power
+		self.player=player
+	def kill(self):
+		self.dead=True
+		self.player.removeCard(self.id)
+	def attack(self,card):
+		global matches
+		#TODO:have defalut for nonexsistant cards
+		card.power=card.power-(self.power*matches[self.name][card.name])
+		if card.power<=0:
+			card.kill()
+		if self.power<=0:
+			self.kill()
+		
+class user():
+	def __init__(self,id):
+		self.id=id
+		self.cards=[]
+		global matches
+		self.newCards(matches,"match_p1")
+	def newCards(self,matches_,matchPack):
+		
+		i=0
+		while(i<30):
+			#self.cards
+			self.cards.append(card(self,matches_.keys()[randint(0,len(matches_.keys()))],matchPack,randint(3,40)))	
+	def getUser(self,users,id):
+		for user in users:
+			if user.id==id:
+				return user
+	def getHand(self,size):
+		return self.cards[len(self.cards)-size:len(self.cards)]
+	def removeCard(self,cardID):
+		i=0
+		for card in self.cards:
+			if(card.id==cardID):
+				self.cards.pop(i)
+				break
+			i+=1
+@app.route("/")
+def hello():
+	return "Welcome to Python Flask!"
+ 
+@app.route('/signUpUser', methods=['POST','GET'])
+def signUpUser():
+	print(request.args["id"])
+	global serverData
+	serverData["users"].append(user(request.args['id']))
+	#password = request.form['password']
+	return json.dumps({'status':'OK'})
+	
+@app.route('/joingame', methods=['POST','GET'])
+def joingame(): 
+	global serverData
+	serverid=0
+	if(len(serverData["games"])==0):
+		serverData["games"].append({"N.players":1,"players":[request.args['id']]})
+		return json.dumps({'status':'OK','serverid':serverid})
+	else:
+		for server in serverData["games"]:	
+			if server["N.players"]>int(request.args["min players"]) and server["N.players"]>request.args["max players"]:
+				server["N.players"]+=1
+				server["players"].append(request.args['id'])
+				return json.dumps({'status':'OK','serverid':serverid})
+			serverid+=1
+		serverData["games"].append({"N.players":1,"players":[request.args['id']]})
+		serverid+=1
+		return json.dumps({'status':'OK','serverid':serverid})
+		
+if __name__ == "__main__":
+	app.run()
+			
---
+++ Assets/web.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+public class web : MonoBehaviour
+{
+
+
+    public string host= "http://127.0.0.1:5000/";
+    void Start()
+    {
+        //host = "http://127.0.0.1:5000/";
+        StartCoroutine(Upload());
+        StartCoroutine(joinGame());
+
+    }
+
+    IEnumerator Upload()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("id=70"));
+        //formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+
+        WWW www = new WWW(host+"signUpUser?id=70");
+        yield return www;
+
+        if (www.error != null)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+
+            Debug.Log(www.text);
+
+        }
+    }
+    IEnumerator joinGame()
+    {
+
+        //formData.Add(new MultipartFormDataSection("id=70"));
+        //formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+
+        WWW www = new WWW(host+"joingame?min%20players=0&id=70");
+        yield return www;
+
+        if (www.error != null)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+
+            Debug.Log(www.text);
+
+        }
+
+    }
+}
